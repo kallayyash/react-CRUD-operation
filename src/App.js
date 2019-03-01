b7c0212e6c4c3ab0c9fea1dc9c7bbd2c0ee979c3
Kallayya <kallayya@ETVMM119853.local>
2019-03-01 17:31:29 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state={
+      title: 'CRUD Using ReactJs ',
+      act: 0,
+      index: '',
+      datas: []
+    }
+  } 
+
+  componentDidMount(){
+    this.refs.name.focus();
+  }
+
+  fSubmit = (e) =>{
+    e.preventDefault();
+    console.log('try');
+
+    let datas = this.state.datas;
+    let name = this.refs.name.value;
+    let contact = this.refs.contact.value;
+    let address = this.refs.address.value;
+
+    if(this.state.act === 0){   //new
+      let data = {
+        name, address , contact
+      }
+      datas.push(data);
+    }else{                      //update
+      let index = this.state.index;
+      datas[index].name = name;
+      datas[index].contact = contact;
+      datas[index].address = address;
+
+    }    
+
+    this.setState({
+      datas: datas,
+      act: 0
+    });
+
+    this.refs.myForm.reset();
+    this.refs.name.focus();
+  }
+
+  fRemove = (i) => {
+    let datas = this.state.datas;
+    datas.splice(i,1);
+    this.setState({
+      datas: datas
+    });
+
+    this.refs.myForm.reset();
+    this.refs.name.focus();
+  }
+
+  fEdit = (i) => {
+    let data = this.state.datas[i];
+    this.refs.name.value = data.name;
+    this.refs.contact.value = data.contact;
+    this.refs.address.value = data.address;
+
+    this.setState({
+      act: 1,
+      index: i
+    });
+
+    this.refs.name.focus();
+  }  
+
   render() {
+    let datas = this.state.datas;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="Maindiv">
+        <h2>{this.state.title}</h2>
+        <form ref="myForm" className="myForm">
+          <input type="text" ref="name" placeholder="your name" className="formField" />
+          <input type="text" ref="contact" placeholder="contact number" className="formField" />
+          <input type="text" ref="address" placeholder="your address" className="formField" />
+          <button onClick={(e)=>this.fSubmit(e)} className="myButton">submit </button>
+        </form>
+        </div>
+        <div className="Childiv">
+        <pre>
+          {datas.map((data, i) =>
+            <tr key={i} className="myList">
+              {i+1}. {data.name},{data.contact}, {data.address}
+              <button onClick={()=>this.fRemove(i)} className="myListButton">remove </button>
+              <button onClick={()=>this.fEdit(i)} className="myListButton">edit </button>
+            </tr>
+          )}
+        </pre>
+        </div>
       </div>
     );
   }
